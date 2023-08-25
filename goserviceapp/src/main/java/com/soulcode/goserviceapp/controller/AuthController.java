ba6655e6989e84feecccc44bbc5b716e17f053f7
BrubsMuller <muller.vendeta@hotmail.com>
2023-08-25 16:21:40 -0300

    Criação das funções de enable e disable na pág users/admin

--- goserviceapp/src/main/java/com/soulcode/goserviceapp/controller/AuthController.java
+++ goserviceapp/src/main/java/com/soulcode/goserviceapp/controller/AuthController.java
@@ -1,8 +1,6 @@
 package com.soulcode.goserviceapp.controller;
 
 
-
-
 import com.soulcode.goserviceapp.domain.Cliente;
 import com.soulcode.goserviceapp.service.AuthService;
 import com.soulcode.goserviceapp.service.UsuarioService;
@@ -29,7 +27,7 @@ public class AuthController {
     @GetMapping(value = "/login")
     public ModelAndView login(@RequestParam(name = "error", required = false) String error) {
         ModelAndView mv = new ModelAndView("login");
-        if(error != null) {
+        if (error != null) {
             mv.addObject("errorMessage", "Erro ao autenticar no sistema. Verifique suas credenciais");
         }
         return mv;
@@ -37,22 +35,22 @@ public class AuthController {
 
     @GetMapping(value = "/cadastro")
     public String cadastro() {
-        return"cadastroCliente" ;
+        return "cadastroCliente";
     }
 
-      @PostMapping(value = "/cadastro")
-      public String cadastrarCliente(Cliente cliente, RedirectAttributes attributes) {
-          try {
-              authService.createCliente(cliente);
-              attributes.addFlashAttribute("successMessage", "Novo cliente cadastrado com sucesso!");
-              return "redirect:/auth/login";
-          } catch (Exception ex) {
-              attributes.addFlashAttribute("errorMessage", "Erro ao cadastrar novo cliente.");
-              return "redirect:/auth/cadastro";
-          }
-      }
+    @PostMapping(value = "/cadastro")
+    public String cadastrarCliente(Cliente cliente, RedirectAttributes attributes) {
+        try {
+            authService.createCliente(cliente);
+            attributes.addFlashAttribute("successMessage", "Novo cliente cadastrado com sucesso!");
+            return "redirect:/auth/login";
+        } catch (Exception ex) {
+            attributes.addFlashAttribute("errorMessage", "Erro ao cadastrar novo cliente.");
+            return "redirect:/auth/cadastro";
+        }
+    }
 
-    @GetMapping(value="/password/new")
+    @GetMapping(value = "/password/new")
     public String alterarSenha() {
         return "alterarSenha";
     }
@@ -67,7 +65,7 @@ public class AuthController {
         try {
             authService.updatePassword(authentication, senhaAtual, senhaNova);
             attributes.addFlashAttribute("successMessage", "Senha alterada.");
-        } catch(UsuarioNaoAutenticadoException | UsuarioNaoEncontradoException | SenhaIncorretaException ex) {
+        } catch (UsuarioNaoAutenticadoException | UsuarioNaoEncontradoException | SenhaIncorretaException ex) {
             attributes.addFlashAttribute("errorMessage", ex.getMessage());
         } catch (Exception ex) {
             attributes.addFlashAttribute("errorMessage", "Erro ao tentar alterar a senha.");
